ec50a70423e2d24da8ec338b53e642a341c76dd8
Dhanush Vanam <vanamdhanush5@gmail.com>
2021-09-17 13:10:34 +0530

    new code

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { RouterModule } from '@angular/router';
+import { DataService } from './services/data.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      {
+        path:'login', component: LoginComponent
+      }
+    ])
+    ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

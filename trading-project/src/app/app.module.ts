8f4b00d9df07a05ca65738ab9834acc02edb07e3
Dhanush Vanam <vanamdhanush5@gmail.com>
2021-09-17 12:55:37 +0530

    dataservice

--- trading-project/src/app/app.module.ts
+++ trading-project/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterModule } from '@angular/router';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,7 @@ import { RouterModule } from '@angular/router';
       }
     ])
     ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- trading-project/src/app/login/login.component.ts
+++ trading-project/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  custodianid: any;
+  password: any;
+  custodianData: any = [];
+  constructor(private http: HttpClient, private dataSvc: DataService) {
+    this.custodianData = this.getData('');
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
+  }
+  getData(url:string){
+    return this.dataSvc.getDataFromApi('');
   }
 
 }
